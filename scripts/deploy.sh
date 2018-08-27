3543cb0ec90130ed3396f5622fb41bbd11554b80
Marqas <marqas36@gmail.com>
2018-08-27 19:06:33 +0530

    some more

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -17,10 +17,11 @@ fi
 
 PLUGIN="amp-woocommerce"
 PROJECT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
-PLUGIN_BUILDS_PATH="$PROJECT_ROOT"
+#PLUGIN_BUILDS_PATH="$PROJECT_ROOT"
+PLUGIN_BUILDS_PATH="https://github.com/ahmedkaludi/amp-woocommerce/archive/"
 PLUGIN_BUILD_CONFIG_PATH="$PROJECT_ROOT/build-cfg"
 #VERSION=$(/usr/bin/php -f "$PLUGIN_BUILD_CONFIG_PATH/utils/get_plugin_version.php" "$PROJECT_ROOT" "$PLUGIN")
-VERSION="0.5"
+VERSION="0.4-BETA"
 ZIP_FILE="$PLUGIN_BUILDS_PATH/$PLUGIN-$VERSION.zip"
 #ZIP_FILE="https://github.com/ahmedkaludi/amp-woocommerce/archive/0.4-BETA.zip"
 #ZIP_FILE="https://travis-ci.org/MARQAS/amp-hide-posts/builds/421068152#L6"
@@ -86,8 +87,8 @@ cd ../
 rm -fR svn-trunk
 
 # Add new version tag
-mkdir svn/tags/$VERSION
-rsync -r -p $PLUGIN/* svn/tags/$VERSION
+# mkdir svn/tags/$VERSION
+# rsync -r -p $PLUGIN/* svn/tags/$VERSION
 
 # Add new files to SVN
 svn stat svn | grep '^?' | awk '{print $2}' | xargs -I x svn add x@
